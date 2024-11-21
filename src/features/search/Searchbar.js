565388cda22c9d15f5a0182b7ffa19eab3b6c13d
Vincent Wong <vincentwco2002@gmail.com>
2024-11-21 10:19:55 +0800

    Add state control to SearchBar

--- src/features/search/Searchbar.js
+++ src/features/search/Searchbar.js
@@ -1,13 +1,23 @@
 import React from "react";
+import { useState } from "react";
 import './Searchbar.css';
 
 function Searchbar() {
+    const [term, setTerm] = useState();
+
+    function handleTermChange(e) {
+        setTerm(e.target.value);
+    }
 
     return (
         <div className="search-div">
             <h1>Reddit Clone</h1>
             <div className="search-container">
-                <input type='text' className="searchbar" />
+                <input type='text' 
+                    className="searchbar" 
+                    placeholder="Search Reddit"
+                    onChange={handleTermChange}
+                    value={term} />
                 <button className="search-button">Search</button>
             </div>
         </div>
