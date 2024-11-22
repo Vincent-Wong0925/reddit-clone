c16fa1eee15d7fd2762be95b4ce35c0217d32af8
Vincent Wong <vincentwco2002@gmail.com>
2024-11-22 11:06:19 +0800

    Add css file to subreddit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
 import Searchbar from './features/search/Searchbar';
+import SubredditList from './features/subreddit/SubredditList';
 
 function App() {
   return (
     <>
       <Searchbar />
+      <SubredditList />
     </>
   );
 }
