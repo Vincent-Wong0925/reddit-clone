52ad19762831db5a17a17d3e5f72a7ddde9b3858
Vincent Wong <vincentwco2002@gmail.com>
2024-11-25 14:53:02 +0800

    Add vote buttons, comment button and their styling

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -1,13 +1,39 @@
 import React from "react";
 import './post.css';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUp, faCircleDown } from "@fortawesome/free-solid-svg-icons";
+import { faCircleUp as farCircleUp, 
+        faCircleDown as farCircleDown,
+        faComment } from "@fortawesome/free-regular-svg-icons";
 
 function Post({post}) {
-    const {title, selftext} = post;
+    const {
+        title, 
+        selftext, 
+        author,
+        ups
+    } = post;
 
     return (
         <div className="post">
+            <p>u/{author}</p>
             <h2>{title}</h2>
             <p>{selftext}</p>
+            <div className="post-footer">
+                <div className="votes">
+                    <button className="up vote-button">
+                        <FontAwesomeIcon className="vote-icon" icon={farCircleUp} />
+                    </button>
+                    <p>{ups}</p>
+                    <button className="down vote-button">
+                        <FontAwesomeIcon className="vote-icon" icon={farCircleDown} />
+                    </button>
+                </div>
+                <button className="comments">
+                    <FontAwesomeIcon className="comments-icon" icon={faComment} />
+                    <p>Comments</p>
+                </button>
+            </div>
         </div>
     );
 }
