6c276aeb90e4b8f7b4802a4086a0d801c0f6b803
Vincent Wong <vincentwco2002@gmail.com>
2024-11-25 10:31:59 +0800

    Add default subreddit image

--- src/features/subreddit/Subreddit.js
+++ src/features/subreddit/Subreddit.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./Subreddit.css";
+import { faRedditAlien } from "@fortawesome/free-brands-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 function Subreddit({name, url, icon}) {
     return (
         <button className="subreddit-button">
-            <img className="subreddit-icon" src={icon} />
+            {icon !== "" ? 
+            <img className="subreddit-icon" src={icon} alt="subreddit icon" /> : 
+            <FontAwesomeIcon className="subreddit-icon" icon={faRedditAlien} />}
             <p className="subreddit-name">{name}</p>
         </button>
     );
