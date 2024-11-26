82f1909194f27dd6ecbc9ba0a7c452b90583c1c7
Vincent Wong <vincentwco2002@gmail.com>
2024-11-26 10:14:25 +0800

    Implement displaying media in posts

--- src/features/post/Post.js
+++ src/features/post/Post.js
@@ -11,14 +11,31 @@ function Post({post}) {
         title, 
         selftext, 
         author,
-        ups
+        ups,
+        subreddit_name_prefixed,
+        thumbnail,
+        is_video,
+        media
     } = post;
+
+    const defaultThumbnail = ['self', 'default', 'nsfw'];
 
     return (
         <div className="post">
+            <h3>{subreddit_name_prefixed}</h3>
             <p>u/{author}</p>
             <h2>{title}</h2>
             <p>{selftext}</p>
+            {
+                is_video?
+                <video className="reddit-video" controls>
+                    <source src={media.reddit_video.fallback_url} />
+                    Video cannot be played.
+                </video> :
+                thumbnail !== "" && !defaultThumbnail.includes(thumbnail)?
+                <img className="thumbnail" src={thumbnail} /> :
+                null
+            }
             <div className="post-footer">
                 <div className="votes">
                     <button className="up vote-button">
